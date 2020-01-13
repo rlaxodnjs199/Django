c4496f6978e115ca00405936a2a115a095ff7302
Taewon Kim <taewonkim@Taewons-MBP.attlocal.net>
2020-01-12 21:05:37 -0500

    add paths

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('<int:qid>/', views.detail, name='detail'),
+  path('<int:qid>/results/', views.results, name='results'),
+  path('<int:qid>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Hello, world.")
+
+def detail(request, qid):
+  return HttpResponse("You're checking QID %s." %qid)
+
+def results(request, qid):
+  response = "You're looking at QID %s."
+  return HttpResponse(response %qid)
+
+def vote(request, qid):
+  return HttpResponse("You're voting on question %s." %qid)
